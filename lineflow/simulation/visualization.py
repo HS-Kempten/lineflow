5da7de2f82b51ab1061cadafc222ea439f90f442
Tobias Windisch <tobias.windisch@hs-kempten.de>
2026-03-25 21:18:31 +0100

    clean up

--- lineflow/simulation/visualization.py
+++ lineflow/simulation/visualization.py
@@ -1,40 +1,10 @@
 import pygame
+import logging
 from queue import Empty
 
-class Viewpoint:
-    """
-    A class to manage the viewpoint for rendering a 2D surface with zoom and pan capabilities.
-    """
 
-    def __init__(
-        self,
-        size=None,
-        position=None,
-        zoom=1,
-    ):
+logger = logging.getLogger(__name__)
 
-        if size is None:
-        	size = (1410, 1000)
-        self.paper = pygame.Surface(size)
-        self.screen = pygame.display.set_mode((1280, 720))
-
-        if position is None:
-            position = (0, 0)
-        
-        self._view = pygame.Vector3(position[0], position[1], zoom)
-
-    def clear(self):
-        self.screen.fill('white')
-        self.paper.fill('white')
-
-    def _draw(self):
-        self.screen.blit(
-            pygame.transform.smoothscale_by(self.paper, self._view.z),
-            (self._view.x,self._view.y),
-        )
-
-    def teardown(self):
-        pygame.quit()
 
 class Visualization:
     
@@ -95,6 +65,12 @@ class Visualization:
                 self.info = item
             elif item['type'] == 'actions':
                 self.actions = item
+            else:
+                logger.warning(
+                    f"Unknown item type: {item['type']}"
+                    "Will not be visualized."
+                )
+
 
     def check_connection(self):
         self.get_from_connection()
