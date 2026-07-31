5a37619a6c48657fa7dfc56cf97a3c15ef2fa4e4
HSMarieK <marie.kraus@stud.hs-kempten.de>
2026-07-31 14:22:38 +0200

    better_view_instantiation_and_minimap_draft

--- lineflow/simulation/new_visualization.py
+++ lineflow/simulation/new_visualization.py
@@ -1,21 +1,18 @@
 import pygame
 
-#dataclass?
 class Window:
-    size = (1280, 720)
-    center = pygame.Vector2(size[0]/2, size[1]/2)
+    size = pygame.Vector2(1280, 720)
+    center = pygame.Vector2(size.x/2, size.y/2)
 
 
 class View:
-    def __init__(self,x=0,y=0,z=1):
+    def __init__(self,x:float=0.0,y:float=0.0,z:float=1.0):
         self.x = x
         self.y = y
         self.z = z
     @property
     def offset(self) -> pygame.Vector2:
         return pygame.Vector2(self.x, self.y)
-#todo: Tobias pls improve instantiation
-view = View()
 
 #class attribute of VisuObject? or something else
 visu_objects = []
@@ -38,14 +35,14 @@ class VisuBlock(VisuObject):
     @property
     def Rect(self) -> pygame.Rect:
         Rect = pygame.Rect(
-            view.x + self.position.x - self.width/2,
-            view.y + self.position.y - self.height/2,
-            self.width,
-            self.height
+            Window.center.x + (view.x + self.position.x - self.width/2)/view.z,
+            Window.center.y + (view.y + self.position.y - self.height/2)/view.z,
+            self.width/view.z,
+            self.height/view.z
         )
         return Rect
     
-    def draw(self, surface) -> None:
+    def draw(self, surface:pygame.Surface, scale:float=1.0) -> None:
         pygame.draw.rect(
             surface,
             self.color,
@@ -61,13 +58,13 @@ class VisuLine(VisuObject):
         super().__init__(position=position)
         self.endpoint = endpoint
 
-    def draw(self, surface) -> None:
+    def draw(self, surface:pygame.Surface, scale:float=1.0) -> None:
         pygame.draw.line(
             surface,
             self.color,
-            view.offset + self.position,
-            view.offset + self.endpoint,
-            width=self.width
+            Window.center + (view.offset + self.position)/view.z,
+            Window.center + (view.offset + self.endpoint)/view.z,
+            width=int(self.width/view.z)
         )
 
 
@@ -76,7 +73,7 @@ class Crosshair(VisuObject):
     width = 10
     color = "red"
 
-    def draw(self, surface) -> None:
+    def draw(self, surface:pygame.Surface) -> None:
         pygame.draw.line(
             surface,
             self.color,
@@ -90,7 +87,63 @@ class Crosshair(VisuObject):
         )
 
 
-def clear(surface) -> None:
+class MiniMap(VisuObject):
+    scale = 0.1
+
+    def __init__(self, position:pygame.Vector2):
+        super().__init__(position=position)
+        self.active = True
+
+    def toggle(self) -> None:
+        self.active = not self.active
+
+    def draw_minimap(self) -> pygame.Surface:
+        return minimap
+
+    def draw(self, surface:pygame.Surface) -> None:
+        surface.blit(self.draw_minimap(), self.poisition)
+        pygame.draw.rect(
+            surface,
+            self.color,
+            self.Rect
+        )
+
+
+def find_line_bounds() -> pygame.Vecotr2:
+    x_positions = []
+    y_positions = []
+    for item in visu_objects:
+        if not isinstance(item, (Crosshair, MiniMap)):
+            x_positions.append(item.position.x)
+            y_positions.append(item.position.y)
+    line_bounds = dict(
+        upper_left=pygame.Vector2(min(x_positions), min(y_positions)),
+        lower_right=pygame.Vector2(max(x_positions), max(y_positions))
+    )
+    return line_bounds
+
+def find_line_size(line_bounds=None) -> pygame.Vector2:
+    if line_bounds is None:
+        line_bounds = find_line_bounds()
+    line_width = line_bounds['lower_right'].x - line_bounds['upper_left'].x
+    line_height = line_bounds['lower_right'].y - line_bounds['upper_left'].y
+    line_center = line_bounds['upper_left'] + (line_width/2,line_height/2)
+    return line_width, line_height, line_center
+
+def set_initial_view() -> View:
+    line_width, line_height, line_center = find_line_size()
+    x = -line_center.x
+    y = -line_center.y
+    x_scalar = line_width / (Window.size.x-100)
+    y_scalar = line_height / (Window.size.y-100)
+    scalar = max(x_scalar,y_scalar)
+    if scalar < 1:
+        z = 1
+    else:
+        z = round(scalar,1)
+    return View(x,y,z)
+
+def clear(surface:pygame.Surface) -> None:
     surface.fill("white")
 
 def check_user_input(dt) -> bool:
@@ -113,16 +166,17 @@ def check_user_input(dt) -> bool:
     view.z = max(0.5,min(10,view.z))
     return True
 
-def draw_objects_of_type(type, surface):
+def draw_objects_of_type(type, surface:pygame.Surface) -> None:
     for obj in visu_objects:
         if isinstance(obj, type):
             obj.draw(surface)
 
-def draw_scene(surface):
+def draw_scene(surface:pygame.Surface) -> None:
     clear(surface)
     draw_objects_of_type(VisuLine, surface)
     draw_objects_of_type(VisuBlock, surface)
     draw_objects_of_type(Crosshair, surface)
+    draw_objects_of_type(MiniMap, surface)
 
 def run() -> None:
     pygame.init()
@@ -141,4 +195,5 @@ if __name__ == '__main__':
         VisuBlock(pygame.Vector2(100*(n+1),100))
     VisuLine(pygame.Vector2(100,100),pygame.Vector2(200,100))
     Crosshair(Window.center)
+    view = set_initial_view()
     run()
